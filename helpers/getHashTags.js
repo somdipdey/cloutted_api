95ab24221896bbcb8be3cd82610101173b7d938c
Somdip Dey <somdip007@gmail.com>
2021-05-15 16:07:18 +0100

    updated gethashtags.js file to incorporate emojis

--- helpers/getHashTags.js
+++ helpers/getHashTags.js
@@ -1,11 +1,14 @@
 const getHashTags = (inputString) => {
-  const regex = /(?:#)(([a-zA-Z\d]+) | \u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]+)/gm;
-  let matches = [];
-  let match;
+  //const regex = /#(\w+|\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]+)/g;
+  const regex = /#\S+/g;
 
-  while ((match = regex.exec(inputString))) {
-    matches.push(match[1].toLowerCase());
-  }
+  
+const matches = (inputString.match(regex)).map(match=> {
+  const hashtag = match.trim().toLowerCase();
+  return hashtag.substring(1, hashtag.length)
+  });
 
   return matches;
 };
+
+//console.log(getHashTags("I love #stackoverflow because #people are very #helpful! #😀😀 😀 #Here"));
