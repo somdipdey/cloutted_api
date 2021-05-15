dc83ee67ee2cc1f6fd0f88c1c64543db52b91886
Somdip Dey <somdip007@gmail.com>
2021-05-15 14:22:00 +0100

    updated getHashTags to get emoji after # as well

--- helpers/getHashTags.js
+++ helpers/getHashTags.js
@@ -1,5 +1,5 @@
 const getHashTags = (inputString) => {
-  let regex = /(?:^|\s)(?:#)([a-zA-Z\d]+)/gm;
+  const regex = /(?:#)(([a-zA-Z\d]+) | \u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]+)/gm;
   let matches = [];
   let match;
 
